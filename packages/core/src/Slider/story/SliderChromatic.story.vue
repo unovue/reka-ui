47ccd2e2b667b14301a0ce470430bb89cc0dc7ad
zernonia <zernonia@gmail.com>
2025-03-15 14:53:34 +0800

    chore: add story

--- packages/core/src/Slider/story/SliderChromatic.story.vue
+++ packages/core/src/Slider/story/SliderChromatic.story.vue
@@ -160,5 +160,69 @@ const controlledRtlMulti = ref([10, 30])
         />
       </SliderRoot>
     </Variant>
+
+    <Variant title="Overflow">
+      <SliderRoot
+        :default-value="[50]"
+        thumb-alignment="overflow"
+        class="relative flex items-center select-none touch-none w-[200px] h-5"
+      >
+        <SliderTrack class="bg-blackA10 relative grow rounded-full h-[3px]">
+          <SliderRange class="absolute bg-white rounded-full h-full" />
+        </SliderTrack>
+        <SliderThumb
+          class="block w-5 h-5 bg-white shadow-[0_2px_10px] shadow-blackA7 rounded-[10px] hover:bg-violet3 focus:outline-none focus:shadow-[0_0_0_5px] focus:shadow-blackA8"
+        />
+      </SliderRoot>
+    </Variant>
+
+    <Variant title="Overflow (RTL)">
+      <SliderRoot
+        :default-value="[50]"
+        thumb-alignment="overflow"
+        dir="rtl"
+        class="relative flex items-center select-none touch-none w-[200px] h-5"
+      >
+        <SliderTrack class="bg-blackA10 relative grow rounded-full h-[3px]">
+          <SliderRange class="absolute bg-white rounded-full h-full" />
+        </SliderTrack>
+        <SliderThumb
+          class="block w-5 h-5 bg-white shadow-[0_2px_10px] shadow-blackA7 rounded-[10px] hover:bg-violet3 focus:outline-none focus:shadow-[0_0_0_5px] focus:shadow-blackA8"
+        />
+      </SliderRoot>
+    </Variant>
+
+    <Variant title="Vertical Overflow">
+      <SliderRoot
+        class="relative flex justify-center items-center select-none touch-none w-5 h-[100px]"
+        :default-value="[50]"
+        thumb-alignment="overflow"
+        orientation="vertical"
+      >
+        <SliderTrack class="bg-blackA10 relative rounded-full w-[3px] h-full">
+          <SliderRange class="absolute bg-white rounded-full w-full" />
+        </SliderTrack>
+        <SliderThumb
+          class="block w-5 h-5 bg-white shadow-[0_2px_10px] shadow-blackA7 rounded-[10px] hover:bg-violet3 focus:outline-none focus:shadow-[0_0_0_5px] focus:shadow-blackA8"
+        />
+      </SliderRoot>
+    </Variant>
+
+    <Variant title="Vertical Overflow (Inverted)">
+      <SliderRoot
+        class="relative flex justify-center items-center select-none touch-none w-5 h-[100px]"
+        :default-value="[50]"
+        thumb-alignment="overflow"
+        orientation="vertical"
+        :inverted="true"
+      >
+        <SliderTrack class="bg-blackA10 relative rounded-full w-[3px] h-full">
+          <SliderRange class="absolute bg-white rounded-full w-full" />
+        </SliderTrack>
+        <SliderThumb
+          class="block w-5 h-5 bg-white shadow-[0_2px_10px] shadow-blackA7 rounded-[10px] hover:bg-violet3 focus:outline-none focus:shadow-[0_0_0_5px] focus:shadow-blackA8"
+        />
+      </SliderRoot>
+    </Variant>
   </Story>
 </template>
