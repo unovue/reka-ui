44cef403cb1fcb146076601d9100cc94aba92ea0
J-Michalek <jaku.michalek@gmail.com>
2025-04-05 08:21:59 +0200

    test(popover-trigger-change-story): remove unnecessary code from the story

--- packages/core/src/Popover/story/PopoverTriggerChange.story.vue
+++ packages/core/src/Popover/story/PopoverTriggerChange.story.vue
@@ -7,8 +7,6 @@ import {
   PopoverTrigger,
 } from '..'
 
-const boundary = ref<HTMLElement>()
-
 const open = ref(false)
 </script>
 
@@ -18,42 +16,34 @@ const open = ref(false)
     :layout="{ type: 'single', iframe: false }"
   >
     <Variant title="default">
-      <div
-        ref="boundary"
-        class="border border-3 border-dashed border-red-600 w-[200px] h-[200px]"
-      >
-        <PopoverRoot v-model:open="open">
-          <PopoverTrigger
-            class="rounded-full w-[35px] h-[35px] inline-flex items-center justify-center text-violet11 bg-white shadow-[0_2px_10px] shadow-blackA7 hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-black cursor-default outline-none"
-            aria-label="Update dimensions"
-            as-child
+      <PopoverRoot v-model:open="open">
+        <PopoverTrigger
+          class="rounded-full w-[35px] h-[35px] inline-flex items-center justify-center text-violet11 bg-white shadow-[0_2px_10px] shadow-blackA7 hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-black cursor-default outline-none"
+          aria-label="Update dimensions"
+          as-child
+        >
+          <button v-if="!open">
+            Closed
+          </button>
+          <button v-else>
+            Open
+          </button>
+        </PopoverTrigger>
+        <PopoverPortal>
+          <PopoverContent
+            :side-offset="5"
+            :style="{
+              boxSizing: 'border-box',
+              borderRadius: '8px',
+              padding: '8px',
+              color: 'white',
+              backgroundColor: 'black',
+            }"
           >
-            <button v-if="!open">
-              Closed
-            </button>
-            <button v-else>
-              Open
-            </button>
-          </PopoverTrigger>
-          <PopoverPortal>
-            <PopoverContent
-              :side-offset="5"
-              :collision-boundary="boundary"
-              :style="{
-                boxSizing: 'border-box',
-                borderRadius: '8px',
-                padding: '8px',
-                color: 'white',
-                backgroundColor: 'black',
-                width: 'var(--reka-popper-available-width)',
-                height: 'var(--reka-popper-available-height)',
-              }"
-            >
-              The content stays correctly positioned event after the buttons are changed.
-            </PopoverContent>
-          </PopoverPortal>
-        </PopoverRoot>
-      </div>
+            The content stays correctly positioned event after the buttons are changed.
+          </PopoverContent>
+        </PopoverPortal>
+      </PopoverRoot>
     </Variant>
   </Story>
 </template>
