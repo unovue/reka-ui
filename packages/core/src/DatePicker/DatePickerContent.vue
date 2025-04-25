23ac0af31a90094d34a4314e7f9a3ad69843f7db
Jack Englund <jack@allocate.co>
2025-04-25 12:00:34 -0500

    #1847 add portal prop to date picker

--- packages/core/src/DatePicker/DatePickerContent.vue
+++ packages/core/src/DatePicker/DatePickerContent.vue
@@ -1,8 +1,11 @@
 <script lang="ts">
-import type { PopoverContentEmits, PopoverContentProps } from '..'
+import type { PopoverContentEmits, PopoverContentProps, PopoverPortalProps } from '..'
+import { computed } from 'vue'
 import { PopoverContent, PopoverPortal, useForwardPropsEmits } from '..'
 
-export interface DatePickerContentProps extends PopoverContentProps {}
+export interface DatePickerContentProps extends PopoverContentProps {
+  portal?: PopoverPortalProps
+}
 export interface DatePickerContentEmits extends PopoverContentEmits {}
 </script>
 
@@ -10,11 +13,15 @@ export interface DatePickerContentEmits extends PopoverContentEmits {}
 const props = defineProps<DatePickerContentProps>()
 const emits = defineEmits<DatePickerContentEmits>()
 
-const forwarded = useForwardPropsEmits(props, emits)
+const propsToForward = computed(() => ({
+  ...props,
+  portal: undefined,
+}))
+const forwarded = useForwardPropsEmits(propsToForward, emits)
 </script>
 
 <template>
-  <PopoverPortal>
+  <PopoverPortal v-bind="portal">
     <PopoverContent
       v-bind="{ ...forwarded, ...$attrs }"
     >
