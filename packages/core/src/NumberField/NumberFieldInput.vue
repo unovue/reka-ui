cc41c3a5fbdd2fdd7838abde70e144b5d1d2d1d8
kricsleo <kricsleo@163.com>
2025-05-26 14:35:03 +0800

    feat: support `invertWheelChange` to invert the wheel change behavior

--- packages/core/src/NumberField/NumberFieldInput.vue
+++ packages/core/src/NumberField/NumberFieldInput.vue
@@ -35,9 +35,9 @@ function handleWheelEvent(event: WheelEvent) {
 
   event.preventDefault()
   if (event.deltaY > 0)
-    rootContext.handleDecrease()
+    rootContext.invertWheelChange.value ? rootContext.handleDecrease() : rootContext.handleIncrease()
   else if (event.deltaY < 0)
-    rootContext.handleIncrease()
+    rootContext.invertWheelChange.value ? rootContext.handleIncrease() : rootContext.handleDecrease()
 }
 
 onMounted(() => {
