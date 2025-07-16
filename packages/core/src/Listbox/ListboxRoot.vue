a328cfeb916959e20afeb161d5bdb935ebedefa4
zernonia <zernonia@gmail.com>
2025-07-16 10:13:49 +0800

    fix: change compositionEnd on listbox

--- packages/core/src/Listbox/ListboxRoot.vue
+++ packages/core/src/Listbox/ListboxRoot.vue
@@ -227,7 +227,7 @@ function onCompositionStart() {
   isComposing.value = true
 }
 function onCompositionEnd() {
-  requestAnimationFrame(() => {
+  nextTick(() => {
     isComposing.value = false
   })
 }
