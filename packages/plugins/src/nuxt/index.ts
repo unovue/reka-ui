4fa3d68e5d8d51355a1057511bfd753e6992c9e8
Damian Głowala <damian.glowala.rebkow@gmail.com>
2025-10-28 17:25:44 +0100

    feat: update nuxt playground

--- packages/plugins/src/nuxt/index.ts
+++ packages/plugins/src/nuxt/index.ts
@@ -11,7 +11,7 @@ export interface ModuleOptions {
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: '@reka-ui/nuxt',
+    name: 'reka-ui/nuxt',
     configKey: 'reka',
     compatibility: {
       nuxt: '>=3.0.0',
--- playgrounds/nuxt/nuxt.config.ts
+++ playgrounds/nuxt/nuxt.config.ts
@@ -1,26 +1,8 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-  hooks: {
-    'components:dirs': (dirs) => {
-      dirs.unshift({
-        path: '../../docs/components/demo',
-        ignore: ['**/css/**'],
-        // this is required else Nuxt will autoImport `.ts` file
-        extensions: ['.vue'],
-        // prefix for your components, eg: UiButton
-        prefix: '',
-        // prevent adding another prefix component by it's path.
-        pathPrefix: true,
-        extendComponent(component) {
-          return {
-            ...component,
-            kebabName: component.kebabName.replace('-tailwind', ''),
-            pascalName: component.pascalName.replace('Tailwind', ''),
-          }
-        },
-      })
-    },
-  },
+  modules: [
+    'reka-ui/nuxt',
+    '@nuxtjs/tailwindcss',
+  ],
   compatibilityDate: '2025-07-15',
 })
