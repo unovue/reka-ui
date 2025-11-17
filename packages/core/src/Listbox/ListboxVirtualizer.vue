663a8832f94e8eb53a2f215f899b6e9dcb609744
Benjamin Canac <canacb1@gmail.com>
2025-11-17 15:45:21 +0100

    feat(ListboxVirtualizer/TreeVirtualizer): handle `estimateSize` as function

--- packages/core/src/Listbox/ListboxVirtualizer.vue
+++ packages/core/src/Listbox/ListboxVirtualizer.vue
@@ -5,7 +5,7 @@ export interface ListboxVirtualizerProps<T extends AcceptableValue = AcceptableV
   /** Number of items rendered outside the visible area */
   overscan?: number
   /** Estimated size (in px) of each item */
-  estimateSize?: number
+  estimateSize?: number | ((index: number) => number)
   /** Text content for each item to achieve type-ahead feature */
   textContent?: (option: T) => string
 }
@@ -64,7 +64,10 @@ const virtualizer = useVirtualizer(
     get scrollPaddingEnd() { return padding.value.end },
     get count() { return props.options.length },
     get horizontal() { return rootContext.orientation.value === 'horizontal' },
-    estimateSize() {
+    estimateSize(index) {
+      if (typeof props.estimateSize === 'function')
+        return props.estimateSize(index)
+
       return props.estimateSize ?? 28
     },
     getScrollElement() { return parentEl.value },
