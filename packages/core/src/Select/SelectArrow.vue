9af0412b884fee65ef62144a115047e17413952e
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2025-12-23 00:19:42 -0500

    fix(Select): always show select arrow and leave display to the select content (#2348)

--- packages/core/src/Select/SelectArrow.vue
+++ packages/core/src/Select/SelectArrow.vue
@@ -7,20 +7,18 @@ export interface SelectArrowProps extends PopperArrowProps {}
 <script setup lang="ts">
 import { PopperArrow } from '@/Popper'
 import { injectSelectContentContext, SelectContentDefaultContextValue } from './SelectContentImpl.vue'
-import { injectSelectRootContext } from './SelectRoot.vue'
 
 const props = withDefaults(defineProps<SelectArrowProps>(), {
   width: 10,
   height: 5,
   as: 'svg',
 })
-const rootContext = injectSelectRootContext()
 const contentContext = injectSelectContentContext(SelectContentDefaultContextValue)
 </script>
 
 <template>
   <PopperArrow
-    v-if="rootContext.open.value && contentContext.position === 'popper'"
+    v-if="contentContext.position === 'popper'"
     v-bind="props"
   >
     <slot />
