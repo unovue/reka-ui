396e2b84cb37801182b395b33dcd5a72cfa68f79
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2026-05-15 00:04:33 +0300

    feat(core): add typed return values for useEmitAsProps
    The useEmitAsProps function now properly types the returned props based on
    the emit function signature, including support for function overloads. This
    improves TypeScript type inference and provides better IDE autocomplete for
    event handlers.
    
    - Added generic type parameter Fn for the emit function type
    - Added type utilities for handling function overloads
    - Result type now accurately reflects the event handler signatures

--- packages/core/src/shared/useEmitAsProps.ts
+++ packages/core/src/shared/useEmitAsProps.ts
@@ -5,19 +5,22 @@ import { camelize, getCurrentInstance, toHandlerKey } from 'vue'
 /**
  * The `useEmitAsProps` function is a TypeScript utility that converts emitted events into props for a
  * Vue component.
+ *
+ * @template Name - The event name string union type.
+ * @template Fn - The emit function type.
+ *
  * @param emit - The `emit` parameter is a function that is used to emit events from a component. It
+ *
  * takes two parameters: `name` which is the name of the event to be emitted, and `...args` which are
  * the arguments to be passed along with the event.
  * @returns The function `useEmitAsProps` returns an object that maps event names to functions that
  * call the `emit` function with the corresponding event name and arguments.
  */
-export function useEmitAsProps<Name extends string>(
-  emit: (name: Name, ...args: any[]) => void,
-) {
+export function useEmitAsProps<Name extends string, Fn extends Function = Function>(emit: ToEmit<Name, Fn>) {
   const vm = getCurrentInstance()
 
   const events = vm?.type.emits as Name[]
-  const result: Record<string, any> = {}
+  const result: EmitAsProps<Fn> = {}
 
   if (!events?.length) {
     console.warn(
@@ -28,5 +31,75 @@ export function useEmitAsProps<Name extends string>(
   events?.forEach((ev) => {
     result[toHandlerKey(camelize(ev))] = (...arg: any) => emit(ev, ...arg)
   })
+
   return result
 }
+
+export type ToEmit<Name extends string, Fn extends Function>
+  = Fn extends (name: infer TName, ...args: any[]) => infer TReturn
+    ? TName extends Name
+      ? [TReturn] extends [void]
+          ? Fn
+          : never
+      : never
+    : never
+
+type OverloadSignature<
+  TArgs extends readonly unknown[],
+  TReturn,
+> = {
+  args: TArgs
+  return: TReturn
+}
+
+// @ts-expect-error - intentionally inherit from generic argument to capture all function overloads
+interface ExtendSignature<T extends object, TArgs extends readonly unknown[], TReturn> extends T {
+  (...args: TArgs): TReturn
+}
+
+type OverloadSignatureTuple<
+  TFunction extends Function,
+  TProps extends object,
+  TSignature extends readonly OverloadSignature<readonly unknown[], unknown>[],
+> = TProps extends TFunction
+  ? TSignature
+  : TFunction extends ExtendSignature<TProps, infer TArgs, infer TReturn>
+    ? OverloadSignatureTuple<
+      TFunction,
+      ExtendSignature<TProps, TArgs, TReturn>,
+      [Props<OverloadSignature<TArgs, TReturn>>, ...TSignature]
+    >
+    : TSignature
+
+export type Props<T extends object> = {
+  [K in keyof T]: T[K]
+} & {}
+
+type ToFunctionSignatureTuple<T extends readonly OverloadSignature<readonly unknown[], unknown>[]> = {
+  [K in keyof T]: T[K] extends OverloadSignature<infer A, infer R>
+    ? (...args: A) => R
+    : never;
+}
+
+type FunctionSignatureTuple<T extends Function> = ToFunctionSignatureTuple<OverloadSignatureTuple<T, Props<T>, readonly []>>
+
+type FunctionSignature<T extends Function> = FunctionSignatureTuple<T>[number]
+
+type UnionToOptional<T> = {
+  [K in T extends any ? keyof T : never]: T extends { [P in K]: any } ? T[K] : never;
+}
+
+type EmitFunction<TName extends string, TArgs extends unknown[], TReturn> = (name: TName, ...args: TArgs) => TReturn
+
+type CamelCase<S extends string>
+  = S extends `${infer T}${'-'}${infer U}` ? `${Lowercase<T>}${Capitalize<CamelCase<U>>}`
+    : S
+
+type HandlerKey<TName extends string> = CamelCase<`on-${TName}`>
+
+type EmitUnion<Emits extends Function>
+  = Emits extends EmitFunction<infer TName, infer TArgs, infer TReturn>
+    ? { [K in HandlerKey<TName>]: (...args: TArgs) => TReturn }
+    : unknown
+
+export type EmitAsProps<T extends Function> = Props<UnionToOptional<EmitUnion<FunctionSignature<T>>>>
