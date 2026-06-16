21df63edb97ac5d6d456ec6c664f29c6ae1082c3
neukinal <neukinal@fh-m.net>
2026-06-16 17:35:08 +0200

    test(Toggle): target button element in assertions

--- packages/core/src/Toggle/Toggle.test.ts
+++ packages/core/src/Toggle/Toggle.test.ts
@@ -1,4 +1,4 @@
-import type { VueWrapper } from '@vue/test-utils'
+import type { DOMWrapper, VueWrapper } from '@vue/test-utils'
 import { mount } from '@vue/test-utils'
 import { beforeEach, describe, expect, it } from 'vitest'
 import { axe } from 'vitest-axe'
@@ -6,37 +6,39 @@ import { Toggle } from '.'
 
 describe('given default Toggle', () => {
   let wrapper: VueWrapper<InstanceType<typeof Toggle>>
+  let button: DOMWrapper<HTMLButtonElement>
 
   beforeEach(() => {
     wrapper = mount(Toggle, {
       attrs: { 'aria-label': 'Toggle italic' },
     })
+    button = wrapper.find('button')
   })
 
   it('should pass axe accessibility tests', async () => {
-    expect(await axe(wrapper.element)).toHaveNoViolations()
+    expect(await axe(button.element)).toHaveNoViolations()
   })
 
   it('should not be toggled yet', () => {
-    expect(wrapper.attributes('data-state')).toBe('off')
+    expect(button.attributes('data-state')).toBe('off')
   })
 
   describe('after toggling', () => {
-    beforeEach(() => {
-      wrapper.trigger('click')
+    beforeEach(async () => {
+      await button.trigger('click')
     })
 
     it('should be toggled on', () => {
-      expect(wrapper.attributes('data-state')).toBe('on')
+      expect(button.attributes('data-state')).toBe('on')
     })
 
     describe('after toggling again', () => {
-      beforeEach(() => {
-        wrapper.trigger('click')
+      beforeEach(async () => {
+        await button.trigger('click')
       })
 
       it('should be toggled off', () => {
-        expect(wrapper.attributes('data-state')).toBe('off')
+        expect(button.attributes('data-state')).toBe('off')
       })
     })
   })
@@ -44,12 +46,14 @@ describe('given default Toggle', () => {
 
 describe('given disabled Toggle', () => {
   let wrapper: VueWrapper<InstanceType<typeof Toggle>>
+  let button: DOMWrapper<HTMLButtonElement>
 
   beforeEach(() => {
     wrapper = mount(Toggle, {
       props: { disabled: true },
       attrs: { 'aria-label': 'Toggle italic' },
     })
+    button = wrapper.find('button')
   })
 
   it('should pass axe accessibility tests', async () => {
@@ -57,21 +61,21 @@ describe('given disabled Toggle', () => {
   })
 
   it('should not be toggled yet', () => {
-    expect(wrapper.attributes('data-state')).toBe('off')
+    expect(button.attributes('data-state')).toBe('off')
   })
 
   describe('try toggling', () => {
-    beforeEach(() => {
-      wrapper.trigger('click')
+    beforeEach(async () => {
+      await button.trigger('click')
     })
 
     it('should be toggled off', () => {
-      expect(wrapper.attributes('data-state')).toBe('off')
+      expect(button.attributes('data-state')).toBe('off')
     })
 
     it('should render disable attributes', () => {
-      expect(wrapper.attributes('data-disabled')).toBe('')
-      expect(wrapper.attributes('disabled')).toBe('')
+      expect(button.attributes('data-disabled')).toBe('')
+      expect(button.attributes('disabled')).toBe('')
     })
   })
 })
