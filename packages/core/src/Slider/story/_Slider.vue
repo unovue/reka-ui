da8ae6d870e744849bb4cf9be8f7154d8147d572
neukinal <neukinal@fh-m.net>
2026-06-16 17:36:12 +0200

    test(Slider): add accessible label

--- packages/core/src/Slider/story/_Slider.vue
+++ packages/core/src/Slider/story/_Slider.vue
@@ -25,6 +25,7 @@ const forwarded = useForwardPropsEmits(props, emits)
       <SliderRange class="absolute bg-white rounded-full h-full" />
     </SliderTrack>
     <SliderThumb
+      aria-label="Test"
       class="block w-5 h-5 bg-white shadow-[0_2px_10px] shadow-blackA7 rounded-[10px] hover:bg-violet3 focus:outline-none focus:shadow-[0_0_0_5px] focus:shadow-blackA8"
     />
   </SliderRoot>
