71f2d65a982ba24b3530bcb67ec4b3b8a1110722
chaw <ahmedchawekimaamri@gmail.com>
2026-09-12 18:15:29 +0100

    fix(ContextMenu): support controlled open state

--- packages/core/src/ContextMenu/ContextMenu.test.ts
+++ packages/core/src/ContextMenu/ContextMenu.test.ts
@@ -1,17 +1,19 @@
 import type { VueWrapper } from '@vue/test-utils'
-import { findByRole } from '@testing-library/vue'
+import { findByRole, queryByRole } from '@testing-library/vue'
 import { mount } from '@vue/test-utils'
 import { beforeEach, describe, expect, it } from 'vitest'
 import { axe } from 'vitest-axe'
 import ContextMenu from './story/_ContextMenu.vue'
+import ControlledContextMenu from './story/_ControlledContextMenu.vue'
+
+globalThis.ResizeObserver = class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
 
 describe('given default ContextMenu', () => {
   let wrapper: VueWrapper<InstanceType<typeof ContextMenu>>
-  globalThis.ResizeObserver = class ResizeObserver {
-    observe() {}
-    unobserve() {}
-    disconnect() {}
-  }
 
   beforeEach(() => {
     document.body.innerHTML = ''
@@ -33,6 +35,33 @@ describe('given default ContextMenu', () => {
 
     it('should render the menu', async () => {
       expect(await findByRole(document.body, 'menu')).toBeTruthy()
+    })
+  })
+})
+
+describe('given controlled ContextMenu', () => {
+  let wrapper: VueWrapper<InstanceType<typeof ControlledContextMenu>>
+
+  beforeEach(() => {
+    document.body.innerHTML = ''
+    wrapper = mount(ControlledContextMenu, { attachTo: document.body })
+  })
+
+  describe('when open state is changed programmatically', () => {
+    it('should render the menu', async () => {
+      await wrapper.find('#open-button').trigger('click')
+
+      expect(await findByRole(document.body, 'menu')).toBeTruthy()
+    })
+
+    it('should close the menu', async () => {
+      await wrapper.find('#open-button').trigger('click')
+
+      expect(await findByRole(document.body, 'menu')).toBeTruthy()
+
+      await wrapper.find('#close-button').trigger('click')
+
+      expect(queryByRole(document.body, 'menu')).toBeNull()
     })
   })
 })
---
+++ packages/core/src/ContextMenu/story/_ControlledContextMenu.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import {
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuRoot,
+  ContextMenuTrigger,
+} from '..'
+
+const open = ref(false)
+</script>
+
+<template>
+  <button
+    id="open-button"
+    @click="open = !open"
+  >
+    {{ open ? 'Close' : 'Open' }}
+  </button>
+  <ContextMenuRoot v-model:open="open">
+    <ContextMenuTrigger> Right click for contextmenu </ContextMenuTrigger>
+
+    <ContextMenuContent>
+      <ContextMenuItem>Item 1</ContextMenuItem>
+      <ContextMenuItem>Item 2</ContextMenuItem>
+      <button
+        id="close-button"
+        @click="open = false"
+      >
+        close
+      </button>
+      <ContextMenuItem>Item 3</ContextMenuItem>
+    </ContextMenuContent>
+  </ContextMenuRoot>
+</template>
